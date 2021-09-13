cda094ddef511e22231bb0caa1965075a500c699
marekbyrtek <marek.byrtekk@gmail.com>
2021-09-13 15:08:03 +0200

    Logika rejestracji i podstrony

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,29 @@
 import React from "react";
 import SignUp from "./SignUp";
 import { Container } from "react-bootstrap";
+import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
 
 const App = () => {
   return (
-    <Container 
+      <Container 
       className="d-flex align-items-center justify-content-center"
       style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <SignUp />
-      </div>
-    </Container>
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route path="/signup" component={SignUp} />
+                <Route path="/login" component={Login} />
+              </Switch>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
   )
 }
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,16 +1,39 @@
-import React, { useRef } from 'react';
-import { Form, Button, Card } from "react-bootstrap";
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from './contexts/AuthContext';
 
 const SignUp = () => {
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmRef = useRef();
+    const { signup } = useAuth();
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState (false);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError("Hasła się nie zgadzają");
+        }
+
+        try {
+            setError("");
+            setLoading(true);
+            await signup(emailRef.current.value, passwordRef.current.value)
+        } catch {
+            setError("Nie można stworzyć konta");
+        }
+        setLoading(false);
+    }
+
     return (
         <>
             <Card>
                 <Card.Body>
                     <h2 className="text-center mb-4">Zarejestruj się</h2>
-                    <Form>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group className="mb-3" id="email">
                             <Form.Label>Email</Form.Label>
                             <Form.Control type="email" ref={emailRef} required />
@@ -23,7 +46,7 @@ const SignUp = () => {
                             <Form.Label>Potwierdź hasło</Form.Label>
                             <Form.Control type="password" ref={passwordConfirmRef} required />
                         </Form.Group>
-                        <Button variant="outline-primary" className="w-100" type="submit">
+                        <Button disabled={loading} variant="outline-primary" className="w-100" type="submit">
                             Zarejestruj się
                         </Button>
                     </Form>
