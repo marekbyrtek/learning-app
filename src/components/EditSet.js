cfe85b354752f565a5a7c8101e13801e82e9fd54
marekbyrtek <marek.byrtekk@gmail.com>
2021-09-24 22:46:10 +0200

    Wstęp do fiszek

--- src/components/EditSet.js
+++ src/components/EditSet.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
-import { Button, Table } from "react-bootstrap";
+import { Table } from "react-bootstrap";
 import { getDatabase, ref, get, child } from "firebase/database";
 import { getAuth } from "firebase/auth";
 import DeleteButton from "./DeleteButton";
@@ -26,9 +26,25 @@ const EditSet = () => {
             });
     })
 
-    if (list === null) {
-        return <h1>Wczytywanie danych</h1>
-    } else if (Object.keys(list).indexOf(set) !== -1) {
+    if (list === null || Object.keys(list).indexOf(set) === -1) {
+        return (
+            <>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Słowo</th>
+                            <th>Definicja</th>
+                            <th>Komentarz</th>
+                            <th>Usuń</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {<AddWord set={set} />}
+                    </tbody>
+                </Table>
+            </>)
+    } else {
         return (
             <>
                 <Table striped bordered hover>
@@ -56,27 +72,6 @@ const EditSet = () => {
                         {<AddWord set={set} />}
                     </tbody>
                 </Table>
-                <Button onClick={handleSet}>Odśwież</Button>
-            </>
-        )
-    } else {
-        return (
-            <>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Słowo</th>
-                            <th>Definicja</th>
-                            <th>Komentarz</th>
-                            <th>Usuń</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {<AddWord set={set} />}
-                    </tbody>
-                </Table>
-                <Button onClick={handleSet}>Odśwież</Button>
             </>
         )
     }
--- src/components/GenerateLearning.js
+++ src/components/GenerateLearning.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import { Button, Card, Container } from "react-bootstrap";
+import { getDatabase, ref, get, child } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
 const GenerateLearning = () => {
     const { set } = useParams();
-    
-    return <h1>Nauka {set}</h1>
+    const [list, setList] = useState(null);
+
+    useEffect(() => {
+        const database = getDatabase();
+        const auth = getAuth();
+        const userId = auth.currentUser.uid;
+        const dbRef = ref(database);
+            get(child(dbRef, `users/${userId}`)).then((snapshot) => {
+                if (snapshot.exists()) {
+                    setList(snapshot.val());
+                } else {
+                console.log("No data available");
+                }
+            }).catch((error) => {
+                console.error(error);
+            });
+    }, [])
+
+    if (list === null) {
+        return null;
+    } else {
+        return (
+            <Container className="d-flex align-items-center justify-content-center" style={{flexDirection: "column"}}>
+                {Object.entries(list[set]).map(([key, value]) => {
+                    return (
+                    <Card className="learning-card" style={{ width: "30rem" }} key={key}>
+                        <h1>{value.word}</h1>
+                        <div className="learning-card-buttons">
+                            <Button>Poprzedni</Button>
+                            <Button>Następny</Button>
+                        </div>
+                    </Card>
+                    )
+                })}
+            </Container>
+        )
+    }   
 }
 
 export default GenerateLearning;
